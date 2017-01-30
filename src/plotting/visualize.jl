af6ff52ddf01e1b8f7e75a0bb8f28f4efb448014
Ben J. Ward <axolotlfan9250@gmail.com>
2017-01-30 20:21:15 +0000

    trying sorting by sum

--- src/plotting/visualize.jl
+++ src/plotting/visualize.jl
@@ -83,7 +83,7 @@ function heatplot_y_order(df, col)
     m = 1
     for sname in level_values
         winvals = df[df[:SeqName] .== sname, col]
-        means[m] = maximum(winvals)
+        means[m] = sum(winvals)
         m += 1
     end
     o = sortperm(means)
