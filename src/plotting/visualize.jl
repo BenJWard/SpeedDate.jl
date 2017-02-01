2f385f3f0d7db85ef0c8dee2ddb0a6db44a02b30
Ben J. Ward <axolotlfan9250@gmail.com>
2017-02-01 15:15:12 +0000

    a

--- src/plotting/visualize.jl
+++ src/plotting/visualize.jl
@@ -115,7 +115,7 @@ function heatplot(df::DataFrame, col::Symbol, ref::String, legend::String)
     return (plot(df, x = :WindowFirst, y = :SeqName, color = col, Geom.rectbin,
          Guide.xlabel("Window Start (bp)"), Guide.ylabel("Sequence name"),
          Guide.colorkey(legend), Coord.cartesian(xmin = 0),
-         Guide.title("$(legend) between $(ref) and other sequences (sliding window)"))#,
+         Guide.title("$(legend) between $(ref) and other sequences (sliding window)"), df)#,
          #Scale.y_discrete(order = o)), df)
 end
 
